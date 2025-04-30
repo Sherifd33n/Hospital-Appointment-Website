e0b4c8ebe3c8a4b1fd007875718ebf39532cc43c
Sherifd33n <111154987+Sherifd33n@users.noreply.github.com>
2025-04-30 11:59:45 +0100

    created the login and signup pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p>
+          CONTACT <span className="text-gray-700 font-semibold">US</span>
+        </p>
+      </div>
 
-export default Contact
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-32 text-sm">
+        <img
+          src={assets.contact_image}
+          alt=""
+          className="w-full md:max-w-[360px]"
+        />
+
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-lg text-gray-600">Our OFFICE</p>
+          <p className="text-gray-500">34, Road Station.</p>
+          <p className="text-gray-500">
+            Tel: +234 000 000 <br /> Email: @gmail.com
+          </p>
+          <p className="font-semibold text-lg text-gray-600">
+            Careers at PRESCRIPTO
+          </p>
+          <p className="text-gray-500">
+            Learn more about our teams and job openings.
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
